0298583e1540ec148bc5905e035cc7179ba7447d
Harsh Dahiya <onlybelieveharsh@gmail.com>
2025-10-31 15:05:42 +0530

    added the Loader component

---
+++ bookit-client/src/components/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="flex items-center justify-center min-h-[400px] bg-transparent">
+      <div className="h-16 w-16 border-4 border-gray-200 border-t-blue-600 rounded-full animate-spin" />
+    </div>
+  );
+};
+
+export default Loader;
